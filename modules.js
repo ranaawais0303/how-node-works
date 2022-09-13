eab95191d1818e8ec881889221fe4b4ce7ad69d5
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-13 05:35:36 -0700

    exports

--- modules.js
+++ modules.js
@@ -6,3 +6,9 @@ const calc1 = new cal();
 console.log(calc1.add(2, 3));
 console.log(calc1.multiply(2, 3));
 console.log(calc1.divide(2, 3));
+
+///////exports
+// const calc2 = require("./test-module-2");
+//by using object distructuring
+const { add, multiply, divide } = require("./test-module-2");
+console.log(multiply(3, 4));
